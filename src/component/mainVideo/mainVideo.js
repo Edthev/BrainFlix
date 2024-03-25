7a2e4f6b30c20c7f9b2cc31a7541a3d4416099b1
Edward Vargas <edthevargas@gmail.com>
2024-03-25 00:04:08 -0400

    connected to backedn

--- src/component/mainVideo/mainVideo.js
+++ src/component/mainVideo/mainVideo.js
@@ -6,82 +6,82 @@ import "./mainVideo.scss";
 import VideosList from "./nextVideos";
 import FeaturedVideo from "./featuredVideo";
 
-const apiURL = "https://unit-3-project-api-0a5620414506.herokuapp.com/";
-const videosURL = apiURL + "videos";
-const apiKey1 = "3b9fd4ca-2853-4da1-9fb4-68f515858c44";
-const apiKeyUrl = "?api_key=" + apiKey1;
-const videosDataCall = videosURL + apiKeyUrl;
+// const apiURL = "https://unit-3-project-api-0a5620414506.herokuapp.com/";
+// const videosURL = apiURL + "videos";
+// const apiKey1 = "3b9fd4ca-2853-4da1-9fb4-68f515858c44";
+// const apiKeyUrl = "?api_key=" + apiKey1;
+// let videosDataCall = videosURL + apiKeyUrl;
+const videosDataCall = "http://localhost:8069/videos";
+const videosURL = "http://localhost:8069/videos";
 const mainVideoID = "84e96018-4022-434e-80bf-000ce4cd12b8";
 
 function Main() {
-  const [featuredVideo, setFeaturedVideo] = useState();
-  const [videos, setVideos] = useState([]);
-  const { videoID } = useParams();
+   const [featuredVideo, setFeaturedVideo] = useState();
+   const [videos, setVideos] = useState([]);
+   const { videoID } = useParams();
 
-  // ! get all videos
-  const getVideosData = async () => {
-    const response = await axios.get(videosDataCall);
-    const videosRawData = response.data;
-    setVideos(videosRawData);
-    // console.log("videosRawData: ", videosRawData);
-  };
-  useEffect(() => {
-    getVideosData();
-  }, []);
+   // ! get all videos
+   const getVideosData = async () => {
+      const response = await axios.get(videosDataCall);
+      const videosRawData = response.data;
+      setVideos(videosRawData);
+      // console.log("videosRawData: ", videosRawData);
+   };
+   useEffect(() => {
+      getVideosData();
+   }, []);
 
-  // ! featured Video
-  const getFeaturedVideo = async (id) => {
-    // console.log("link: ", `${videosURL}/${id}/${apiKeyUrl}`);
-    const response = await axios.get(`${videosURL}/${id}/${apiKeyUrl}`);
-    const featuredVideoData = response.data;
-    // console.log("response: ", featuredVideoData);
-    setFeaturedVideo(featuredVideoData);
-  };
-  console.log(videoID);
-  // * change home page to video 1
-  useEffect(() => {
-    if (videoID) {
-      getFeaturedVideo(videoID);
-      console.log(featuredVideo);
-    } else {
-      getFeaturedVideo(mainVideoID);
-    }
-  }, [videoID]);
+   // ! featured Video
+   const getFeaturedVideo = async (id) => {
+      // console.log("link: ", `${videosURL}/${id}/${apiKeyUrl}`);
+      const response = await axios.get(`${videosURL}/${id}`);
+      const featuredVideoData = response.data;
+      // console.log("response: ", featuredVideoData);
+      setFeaturedVideo(featuredVideoData);
+   };
+   console.log(videoID);
+   // * change home page to video 1
+   useEffect(() => {
+      if (videoID) {
+         getFeaturedVideo(videoID);
+         console.log(featuredVideo);
+      } else {
+         getFeaturedVideo(mainVideoID);
+      }
+   }, [videoID]);
 
-  // ! filtered videos
-  const filteredVideos = videos.filter(
-    (video) => video.id !== featuredVideo.id
-  );
-  // console.log(filteredVideos);
+   // ! filtered videos
+   const filteredVideos = videos.filter((video) => video.id !== featuredVideo.id);
+   // console.log(filteredVideos);
 
-  // const handleVideoClick = (selectedVideo) => {
-  //   console.log(selectedVideo);
-  //   const videoDetails = videosDataCall.find(
-  //     (video) => video.id === selectedVideo.id
-  //   );
-  //   setFeaturedVideo(videoDetails);
-  //   console.log(filteredVideos);
-  // };
-  // console.log(featuredVideo);
-  return (
-    <div className="video">
-      {/* spacing for header */}
-      <div className="empty">t1</div>
-      <div className="empty">t2</div>
-      <div className="empty">t3</div>
-      <div className="empty">t4</div>
-      <div className="empty">t5</div>
-      <div className="empty">t6</div>
-      <div className="empty">t7</div>
-      <div className="video__mainVideo">
-        {/* main video image */}
-        <FeaturedVideo video={featuredVideo} />
+   // const handleVideoClick = (selectedVideo) => {
+   //   console.log(selectedVideo);
+   //   const videoDetails = videosDataCall.find(
+   //     (video) => video.id === selectedVideo.id
+   //   );
+   //   setFeaturedVideo(videoDetails);
+   //   console.log(filteredVideos);
+   // };
+   // console.log(featuredVideo);
+   return (
+      <div className="video">
+         {/* spacing for header */}
+         <div className="empty">t1</div>
+         <div className="empty">t2</div>
+         <div className="empty">t3</div>
+         <div className="empty">t4</div>
+         <div className="empty">t5</div>
+         <div className="empty">t6</div>
+         <div className="empty">t7</div>
+         <div className="video__mainVideo">
+            {/* main video image */}
+            <FeaturedVideo video={featuredVideo} />
+         </div>
+         <div className="nextVideos">
+            <VideosList videos={filteredVideos} video={featuredVideo} />
+         </div>
       </div>
-      <div className="nextVideos">
-        <VideosList videos={filteredVideos} video={featuredVideo} />
-      </div>
-    </div>
-  );
+   );
 }
 // Main();
 
--- src/component/upload/upload.js
+++ src/component/upload/upload.js
@@ -1,64 +1,86 @@
 import uploadImage from "../../assets/Images/Upload-video-preview.jpg";
 import Upload from "../../assets/Icons/upload.svg";
 import "./upload.scss";
+import axios from "axios";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 let main = () => {
-  return (
-    <div className="uploadPage">
-      <div className="empty">t1</div>
-      <div className="empty">t2</div>
-      <div className="empty">t3</div>
-      <div className="empty">t4</div>
-      <div className="empty">t5</div>
-      <div className="empty">t6</div>
-      <div className="empty">t7</div>
-      <h1 className="uploadPage__title">Upload Video</h1>
-      <div className="border hiddenMobile"></div>
-      <form className="uploadPage__form">
-        <div class="right">
-          <div className="desktop">
-            <h3 className="form__subtitle">VIDEO THUMBNAIL</h3>
-            <div className="form__imageDiv">
-              <img className="form__image" src={uploadImage} />
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      console.log("title: ", e.target.title.value, "\nDesc: ", e.target.description.value);
+      let data = JSON.stringify({
+         title: e.target.title.value,
+         image: uploadImage,
+         description: e.target.description.value,
+      });
+      let config = {
+         method: "post",
+         url: "http://localhost:8069/upload",
+         data: data,
+         headers: {
+            "Content-Type": "application/json",
+         },
+      };
+      axios.request(config);
+      e.target.reset();
+   };
+
+   return (
+      <div className="uploadPage">
+         <div className="empty">t1</div>
+         <div className="empty">t2</div>
+         <div className="empty">t3</div>
+         <div className="empty">t4</div>
+         <div className="empty">t5</div>
+         <div className="empty">t6</div>
+         <div className="empty">t7</div>
+         <h1 className="uploadPage__title">Upload Video</h1>
+         <div className="border hiddenMobile"></div>
+         <form onSubmit={handleSubmit} className="uploadPage__form">
+            <div class="right">
+               <div className="desktop">
+                  <h3 className="form__subtitle">VIDEO THUMBNAIL</h3>
+                  <div className="form__imageDiv">
+                     <img className="form__image" src={uploadImage} />
+                  </div>
+               </div>
+               <div className="desktop">
+                  <div className="form__inputs">
+                     <div className="inputTitle">
+                        <label for="Video Title">TITLE YOUR VIDEO</label>
+                        <input
+                           type="text"
+                           id="title"
+                           name="videoTitle"
+                           placeholder="Add a title to your video"
+                        />
+                     </div>
+                     <div className="inputDescription">
+                        <label for="Video Title">ADD A VIDEO DESCRIPTION</label>
+                        <input
+                           type="text"
+                           id="description"
+                           name="videoDescription"
+                           placeholder="Add a description to your video"
+                        />
+                     </div>
+                  </div>
+               </div>
             </div>
-          </div>
-          <div className="desktop">
-            <div className="form__inputs">
-              <div className="inputTitle">
-                <label for="Video Title">TITLE YOUR VIDEO</label>
-                <input
-                  type="text"
-                  id="title"
-                  name="videoTitle"
-                  placeholder="Add a title to your video"
-                />
-              </div>
-              <div className="inputDescription">
-                <label for="Video Title">ADD A VIDEO DESCRIPTION</label>
-                <input
-                  type="text"
-                  id="description"
-                  name="videoDescription"
-                  placeholder="Add a description to your video"
-                />
-              </div>
+            <div className="border hiddenMobile"></div>
+            <div className="Buttons">
+               <div>
+                  <img className="uploadForm__img" src={Upload} />
+                  <button className="form__button">Publish</button>
+               </div>
+               <div>
+                  <Link to="/">
+                     <button className="form__cancelButton">Cancel</button>
+                  </Link>
+               </div>
             </div>
-          </div>
-        </div>
-        <div className="border hiddenMobile"></div>
-        <div className="Buttons">
-          <div>
-            <img className="uploadForm__img" src={Upload} />
-            <button className="form__button">Publish</button>
-          </div>
-          <div>
-            <Link to="/">
-              <button className="form__cancelButton">Cancel</button>
-            </Link>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+         </form>
+      </div>
+   );
 };
 export default main;
